fd0bf7813a409c32d0000a2e8c8e8afbab2994c7
teserax <67872659+teserax@users.noreply.github.com>
2022-02-28 13:09:03 +0200

    add json

--- metod.go
+++ metod.go
@@ -1,32 +1,63 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"log"
+	"os"
 )
 
 type Persona struct {
 	Id      string
-	Fio     string
+	FIO     string
 	Address string
-	Product Product
+	// Product Product
 }
 
-//type Adress struct {
-//	City   string
-//	street string
-//}
-type Product struct {
-	Name     string
-	Quantity int
-	Price    string
+// type Product struct {
+// 	Name     string
+// 	Quantity int
+// 	Price    string
+// }
+
+func newPersona(Id, FIO, Address string) Persona {
+	return Persona{
+		Id:      Id,
+		FIO:     FIO,
+		Address: Address,
+	}
 }
 
 func main() {
+	// open the file
+	file, err := os.Open("test/1.txt")
 
-	fContent, err := ioutil.ReadFile("1.txt")
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error when opening file: %s", err)
 	}
-	fmt.Println(string(fContent))
+	fileScanner := bufio.NewScanner(file)
+	b := [][]byte{}
+	for fileScanner.Scan() {
+		row := ""
+		// fmt.Println(fileScanner.Text())
+		row = fileScanner.Text()
+		a := []byte(row)
+
+		b = append(b, a)
+	}
+	// handle first encountered error while reading
+	if err := fileScanner.Err(); err != nil {
+		log.Fatalf("Error while reading file: %s", err)
+	}
+
+	file.Close()
+
+	var persona1, persona2, persona3 Persona
+	err = json.Unmarshal(b[0], &persona1)
+	err = json.Unmarshal(b[1], &persona2)
+	err = json.Unmarshal(b[2], &persona3)
+
+	persona4 := newPersona("", "", "")
+	fmt.Println(persona1, persona2, persona3, persona4)
 }
