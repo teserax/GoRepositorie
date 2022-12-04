33c141ed630e7571f105e53d5c2cd03508e6d8a3
Ceapa Vladimir <steeles80@gmail.com>
2022-12-04 21:56:41 +0200

    info Pacient

--- currency/currency_test.go
+++ currency/currency_test.go
@@ -2,20 +2,21 @@ package main
 
 import "testing"
 
-type Sum struct {
+type Test struct {
 	s, expected string
 }
 
-var summa = []Sum{
-	Sum{"EUR 1.203,09", "EUR 1.203,09"},
-	Sum{"$30.65", "$30.65"},
-	Sum{"$30.65 ($13.74 / Pound)", "$30.65"},
-	//Sum{"$1,000,010.46", "$1,000,010.46"},
-	Sum{"+&nbsp;EUR 18,00&nbsp;di&nbsp;spedizione", "EUR 18,00"},
+var denominationTest = []Test{
+	Test{"EUR 1.203,09", "EUR 1.203,09"},
+	Test{"$30.65", "$30.65"},
+	Test{"$30.65 ($13.74 / Pound)", "$30.65"},
+	//Test{"$1,000,010.46", "$1,000,010.46"},
+	Test{"+&nbsp;EUR 18,00&nbsp;di&nbsp;spedizione", "EUR 18,00"},
+	Test{"MP3", ""},
 }
 
 func TestCurrency(t *testing.T) {
-	for i, test := range summa {
+	for i, test := range denominationTest {
 		if col := currency(test.s); col != test.expected {
 			t.Errorf("%d.Expected '%s', but got '%s'", i, test.expected, col)
 		}
